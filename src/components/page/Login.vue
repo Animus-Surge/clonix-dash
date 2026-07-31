3588000ca767c80367b880f23d8ac5656a371623
Surge <esfloyd9341@gmail.com>
2026-07-31 12:33:17 -0400

    Add login page

---
+++ src/components/page/Login.vue
@@ -0,0 +1,12 @@
+<template>
+<!--Page intentionally left blank.-->
+</template>
+
+<script setup lang="ts">
+// TODO: allow this page to function normally with the following flag set to false
+const FORCE_SSO = true
+const SSO_URI = ''
+
+
+
+</script>
